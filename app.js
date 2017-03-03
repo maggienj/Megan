012c68f6ce1b4ac01c93a8f38d619a78c4882df0
smchow <sunita.m.chowdhury@gmail.com>
2017-03-03 17:51:22 -0500

    removed unused package

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var underscore = require('underscore');
-var formidable = require("formidable");
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
