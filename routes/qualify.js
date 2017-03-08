3ab10db0afa8c03eacf5738933e50b9080da18ac
smchow <sunita.m.chowdhury@gmail.com>
2017-03-08 09:57:30 -0500

    simplefied the code to find the max salary limit

--- routes/qualify.js
+++ routes/qualify.js
@@ -20,31 +20,8 @@ router.post('/', function (req, res) {
 
   var myData = require('../lkpData.json');
   var myFilteredData = lodash.filter(myData, {"Type": "Moderate"});
-  var myPersonVal1 = lodash.mapValues(myFilteredData, '1Person');
-  var myPersonVal15 = lodash.mapValues(myFilteredData, '1.5Person');
-  var myPersonVal2 = lodash.mapValues(myFilteredData, '2Person');
-  var myPersonVal3 = lodash.mapValues(myFilteredData, '3Person');
-  var myPersonVal4 = lodash.mapValues(myFilteredData, '4Person');
-  var myPersonVal45 = lodash.mapValues(myFilteredData, '4.5Person');
-  var myPersonVal5 = lodash.mapValues(myFilteredData, '5Person');
-  var myPersonVal6 = lodash.mapValues(myFilteredData, '6Person');
-  var myPersonVal7 = lodash.mapValues(myFilteredData, '7Person');
-  var myPersonVal8 = lodash.mapValues(myFilteredData, '8Person');
   console.log(myFilteredData);
 
-
-  /*
-   console.log(myPersonVal1);
-   console.log(myPersonVal15);
-   console.log(myPersonVal2);
-   console.log(myPersonVal3);
-   console.log(myPersonVal4);
-   console.log(myPersonVal45);
-   console.log(myPersonVal5);
-   console.log(myPersonVal6);
-   console.log(myPersonVal7);
-   console.log(myPersonVal8);
-   */
 
   /* var cnstHhold = "'"+numHhold+'Person' +"'";
    console.log(cnstHhold);
@@ -58,44 +35,9 @@ router.post('/', function (req, res) {
    var unsData = underscore.pluck(myFilteredData,"'"+numHhold+'Person' +"'");
    console.log(unsData);
    */
-    console.log("numHhold = " + numHhold + ", numSalary is " + numSalary);
-    console.log(req.body);
-  var maxSalLimit;
-  switch (numHhold) {
-    case '1':
-      maxSalLimit = myPersonVal1;
-      break;
-    case '1.5':
-      maxSalLimit = myPersonVal15;
-      break;
-    case '2':
-      maxSalLimit = myPersonVal2;
-      break;
-    case '3':
-      maxSalLimit = myPersonVal3;
-      break;
-    case '4':
-      maxSalLimit = myPersonVal4;
-      break;
-    case '4.5':
-      maxSalLimit = myPersonVal45;
-      break;
-    case '5':
-      maxSalLimit = myPersonVal5;
-      break;
-    case '6':
-      maxSalLimit = myPersonVal6;
-      break;
-    case '7':
-      maxSalLimit = myPersonVal7;
-      break;
-    case '8':
-      maxSalLimit = myPersonVal8;
-      break;
-
-    default:
-      maxSalLimit = '0';
-  }
+  console.log("numHhold = " + numHhold + ", numSalary is " + numSalary);
+  var maxSalLimit = lodash.mapValues(myFilteredData, numHhold+'Person');
+  
 // This route receives the posted form.
 // As explained above, usage of 'body-parser' means
 // that `req.body` will be filled in with the form elements
