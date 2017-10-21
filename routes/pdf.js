7b42c018f588c7f1ec17b4c7f8f1895d4880a99d
Michael Reca <michael.lawrence.reca@gmail.com>
2017-10-21 15:18:45 -0400

    various templates added

--- routes/pdf.js
+++ routes/pdf.js
@@ -5,8 +5,21 @@ var router = express.Router();
 router.post('/', function(req, res, next) {
   	var pdfFillForm = require('pdf-fill-form');
 	var fs = require('fs');
-	console.log(Object.keys(req.body))	
-	pdfFillForm.write('affPDF/AffordableHousing_Application sales-Fields.pdf', { "cell": req.body.phone }, { "save": "pdf" } )
+
+	pdfFillForm.write('affPDF/AffordableHousing_Application sales-Fields.pdf',
+		{
+			"name": req.body.name,
+			"address": req.body.address,
+			"state": req.body.state,
+			"city": req.body.city,
+			"zip": req.body.zip,
+			"home": req.body.home,
+			"work": req.body.work,
+			"cell": req.body.cell,
+			"email": req.body.email
+		},
+		{ "save": "pdf" }
+	 )
 	.then(function(result) {
 	    fs.writeFile("affPDF/test123.pdf", result, function(err) {
 	        if(err) {
