5fab5fa27b93dda022f9e720baed6b04c1c70522
Michael Reca <michael.lawrence.reca@gmail.com>
2017-10-21 18:11:42 -0400

    generated json object string for use in model

--- ruby_scripts/all_fields_to_common_and_specific.rb
+++ ruby_scripts/all_fields_to_common_and_specific.rb
@@ -1,4 +1,5 @@
 require "csv"
+require "json"
 
 file = File.open("app_fields.csv", "r")
 file_text = file.read
@@ -50,3 +51,37 @@ CSV.open("common_and_specific_fields.csv", "w+") do |csv|
 		csv << specific_fields[k].uniq.sort
 	end
 end
+
+csv_array = []
+json_hash_array = []
+all_fields.each do |f|
+
+	csv_row = [f]
+	real_headers.each do |h|
+		csv_row << (fields[h].include?(f) ? h : nil)
+	end
+	csv_row.compact!
+	csv_array << csv_row
+	
+	mini_hash = {}
+	mini_hash["type"] = "text"
+	mini_hash["label"] =""
+	mini_hash["value"] = f
+	mini_hash["requiredBy"] = csv_row.slice(1, csv_row.count)
+	json_hash_array << mini_hash
+
+end
+
+CSV.open("fields_belong_to.csv", "w+") do |csv|
+	csv << ["field_name", "needed_by"]
+	csv_array.each do |row|
+		csv << row
+	end
+end
+
+json_array_string = json_hash_array.map {|x| x.to_s}.join(",\n").gsub("=>", ":")
+json_array_string = ["[\n", json_array_string, "\n]"].join
+
+json_file = File.open("fields_belong_to.json", "w+")
+json_file.write json_array_string
+json_file.close
